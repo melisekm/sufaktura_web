bdc32c78c8c1079bff46fe78bb1cb94018667a80
melisekm <46221266+melisekm@users.noreply.github.com>
2021-08-17 00:40:22 +0200

    cleanup

--- resources/frontend/src/redux/slices/customers.js
+++ resources/frontend/src/redux/slices/customers.js
@@ -1,5 +1,4 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {deleteCustomer, getCustomers, updateCustomer, createCustomer} from "../thunks/customers-thunks";
 
 const initialModalErrors = {
     "name": false,
